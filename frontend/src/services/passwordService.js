b267f59dc50d20d4c72921c76adc39329366555f
vryc9 <enzo47volpato@gmail.com>
2024-11-28 14:44:56 +0100

    password service

--- frontend/src/services/passwordService.js
+++ frontend/src/services/passwordService.js
@@ -8,7 +8,7 @@ class PasswordService {
         xhr.addEventListener('load', callback);
         xhr.open('POST', 'http://127.0.0.1:5000/vaults');
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(body);
+        xhr.send(body) ;
     }
 }
 
