32d16c3184baa670edd311c766e704fd11b86497
Prop <51509630+Propleek@users.noreply.github.com>
2024-11-28 14:08:51 +0100

    écran ajout de mot de passe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import Home from './pages/home/Home';
 import Password from './pages/passwordList/password'
+import AddPassword from './pages/addPassword/addPassword';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
       <Route path='/passwords' element={ <Password />}/>
+      <Route path='/addpassword' element={ <AddPassword />}/>
     </>
   )
 );
---
+++ frontend/src/pages/addPassword/addPassword.jsx
@@ -0,0 +1,65 @@
+import NavBar from '../../components/navbar/NavBar'
+import './addPassword.scss'
+import { useState } from 'react'
+
+function AddPassword(){
+
+    const [formData, setFormData] = useState({
+        website: '',
+        username: '',
+        password: ''
+      });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData, 
+          [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        console.log(formData); 
+      };
+    return(
+        <div className="addPassword">
+            <NavBar></NavBar>
+            <div className="content">
+                <form onSubmit={handleSubmit}>
+                    <h1><center>New password</center></h1>
+                    <div className="inputs">
+                        <div className="input">
+                            <label htmlFor="username">Website url</label>
+                            <input 
+                                type="text" 
+                                name='website'
+                                value={formData.website}
+                                onChange={handleChange}/>
+                        </div>
+                        <div className="input">
+                            <label htmlFor="password">Username</label>
+                            <input 
+                                type="text" 
+                                name='username'
+                                value={formData.username}
+                                onChange={handleChange}/>
+                        </div>
+                        <div className="input">
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="password" 
+                                name='password'
+                                value={formData.password}
+                                onChange={handleChange}/>
+                        </div>
+                    </div>
+                    <button id='log-in-btn'>Ajouter</button>
+                </form>
+            </div>
+           
+        </div>
+    )
+}
+
+export default AddPassword
