b7b283f365291224eb258935b3dec08f6b628b83
vryc9 <enzo47volpato@gmail.com>
2024-11-28 15:31:30 +0100

    change vault model

--- backend/models/vault.py
+++ backend/models/vault.py
@@ -2,6 +2,6 @@ from extensions import db
 
 class Vault(db.Model) : 
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(80), unique=True, nullable=False)
-    website = db.Column(db.String(80), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    website = db.Column(db.String(255), unique=False, nullable=False) 
+    username = db.Column(db.String(255), unique=False, nullable=False)
